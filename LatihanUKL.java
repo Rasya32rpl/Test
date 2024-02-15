768a22826f5ca52cd87cfe0d58dfd658553bb8f0
unknown <rasya_rafael32rpl@student.smktelkom-mlg.sch.id>
2024-02-15 18:16:55 +0700

    Second file

---
+++ LatihanUKL.java
@@ -0,0 +1,33 @@
+package latihan.ukl;
+import java.util.Scanner;
+public class LatihanUKL {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("ID 1");
+        System.out.println("ID 2");
+        System.out.println("ID 3");
+        System.out.println("ID 4");
+        System.out.print("Masukkan ID : ");
+        int ID = input.nextInt();
+        String nama[] = {"Galuh","Indro","Jedi","Kanu"};
+        String alamat[] = {"Sawojajar","Kedung Kandang","Ijen","Dinoyo"};
+        String golongan[] = {"1","3","2","3"};
+        if (ID) {
+            System.out.println(alamat[0]+golongan[0]);
+        }
+        }
+        switch (id)
+            case 1 :
+                System.out.println("Masukkan pemakaian (/kwh) : ");
+                int tagihan = input.nextInt();
+            case 2 :
+                
+            case 3 :
+                
+            case 4 : 
+                
+        }
+    }
+   
+}
+
